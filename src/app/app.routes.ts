f5d88852ab00c04affc320f4d768ae4cfbb5d18e
warmijunta <squad24.innovabcp@gmail.com>
2025-03-27 10:18:14 -0500

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    }, 
+    {
+        path: '**',
+        component: NotfoundComponent
+    },  
+];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
